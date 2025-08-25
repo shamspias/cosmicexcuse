b2e31a0f5b8f67bf2129a24ac20a7ef7087064c2
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-08-25 21:42:01 +0300

    feat: add custom exceptions module

---
+++ cosmicexcuse/exceptions.py
@@ -0,0 +1,33 @@
+"""
+Custom exceptions for CosmicExcuse package.
+"""
+
+
+class CosmicExcuseError(Exception):
+    """Base exception for all CosmicExcuse errors."""
+    pass
+
+
+class LanguageNotSupportedError(CosmicExcuseError):
+    """Raised when an unsupported language is requested."""
+    pass
+
+
+class DataLoadError(CosmicExcuseError):
+    """Raised when data files cannot be loaded."""
+    pass
+
+
+class InvalidCategoryError(CosmicExcuseError):
+    """Raised when an invalid category is specified."""
+    pass
+
+
+class MarkovChainError(CosmicExcuseError):
+    """Raised when Markov chain generation fails."""
+    pass
+
+
+class FormatterError(CosmicExcuseError):
+    """Raised when formatting fails."""
+    pass
