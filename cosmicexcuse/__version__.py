8507da363c989a9b4218ed07945f0a876928aad0
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-08-25 21:41:17 +0300

    chore: add version management module

---
+++ cosmicexcuse/__version__.py
@@ -0,0 +1,8 @@
+"""Version information for CosmicExcuse package."""
+
+__version__ = "1.0.0"
+__author__ = "Shamsuddin Ahmed"
+__email__ = "info@shamspias.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025 Shamsuddin Ahmed"
+__status__ = "Beta"
