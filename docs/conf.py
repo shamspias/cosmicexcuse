946ef95eb71d18d382e9b39bc9537d992aad62fb
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-08-28 11:56:26 +0300

    docs: add sphinx conf for docs

---
+++ docs/conf.py
@@ -0,0 +1,95 @@
+"""
+Configuration file for the Sphinx documentation builder.
+"""
+
+import os
+import sys
+from datetime import datetime
+
+# Add project root to path
+sys.path.insert(0, os.path.abspath('..'))
+
+# Import version
+from cosmicexcuse.__version__ import __version__
+
+# -- Project information -----------------------------------------------------
+
+project = 'CosmicExcuse'
+copyright = f'{datetime.now().year}, Shamsuddin Ahmed'
+author = 'Shamsuddin Ahmed'
+release = __version__
+version = __version__
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = None
+html_favicon = None
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'both',
+    'style_external_links': True,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# -- Extension configuration -------------------------------------------------
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+autodoc_typehints = 'description'
+autodoc_mock_imports = []
+
+# Napoleon settings (for Google/NumPy docstrings)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_use_keyword = True
+napoleon_preprocess_types = False
+
+# Intersphinx mapping (link to other projects' documentation)
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'pytest': ('https://docs.pytest.org/en/stable/', None),
+}
+
+# Todo extension
+todo_include_todos = True
